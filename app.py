c85ae722a5d353d8d0a5975a6b432b54406c88ae
Iksha-Goomber <goomberiksha@gmail.com>
2026-06-12 16:41:14 +0530

    Add grayscale image conversion

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Photo Editor")
-st.write("My first streamlit project")
+st.write("Upload an image")
+uploaded_file = st.file_uploader("Upload image")
+
+if (uploaded_file != None):
+    image = Image.open(uploaded_file)
+    st.image(image)
+
+button = st.button("Convert to Grayscale")
+
+if button == True:
+    grayscale_image = image.convert("L")
+    st.image(grayscale_image)
