40b81967060009b72e4b4f9e491599641881c81a
Iksha-Goomber <goomberiksha@gmail.com>
2026-06-12 18:46:30 +0530

    Add passport size and background removal features

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from PIL import Image
+import io
+from rembg import remove
+import cv2
+import tempfile
+
 
 st.title("Photo Editor")
 st.write("Upload an image")
@@ -9,8 +14,50 @@ if (uploaded_file != None):
     image = Image.open(uploaded_file)
     st.image(image)
 
-button = st.button("Convert to Grayscale")
+    option = st.selectbox(
+    "Choose Operation",
+    ["Choose Operation", "Convert to Grayscale", "Passport Size", "Remove Background"]
+    )
 
-if button == True:
-    grayscale_image = image.convert("L")
-    st.image(grayscale_image)
+    if option == "Convert to Grayscale":
+        grayscale_image = image.convert("L")
+        st.image(grayscale_image)
+
+        buffer = io.BytesIO()
+        grayscale_image.save(buffer, format="PNG")
+
+        st.download_button(
+        "Download Grayscale Image",
+        data=buffer.getvalue(),
+        file_name="grayscale_image.png",
+        mime="image/png")
+
+    if option == "Passport Size":
+        passport_image = image.resize((413, 531))
+        st.image(passport_image)
+
+        buffer = io.BytesIO()
+        passport_image.save(buffer, format="PNG")
+
+        st.download_button(
+        "Download Passport sized Image",
+        data=buffer.getvalue(),
+        file_name="passport_image.png",
+        mime="image/png")
+
+    if option == "Remove Background":
+            st.write("Removing background...")
+            output = remove(image)
+            st.image(output)
+
+            buffer = io.BytesIO()
+            output.save(buffer, format="PNG")
+
+            st.download_button(
+            "Download",
+            data=buffer.getvalue(),
+            file_name="output.png",
+            mime="image/png")
+
+
+
